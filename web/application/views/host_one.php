20fea4c516b005230e60aa0f8cd8c2cff573128c
vvoody <ydoovv@gmail.com>
2012-01-16 12:03:01 +0800

    link typos, all are 'device'.

--- web/application/views/host_one.php
+++ web/application/views/host_one.php
@@ -7,9 +7,9 @@ require_once 'navigation.php';
 echo "<h1 class=\"heading\">Name of the Host</h1>";
 echo "<ul>";
 echo "<li class=\"dev\">" . anchor("device/showall/$host_id", 'Devices') . "</li>";
-echo "<li class=\"sto\">". anchor("device/showall/$host_id", 'Storage') . "</li>";
-echo "<li class=\"isft\">" . anchor("device/showall/$host_id", 'Installed Software') . "</li>";
-echo "<li class=\"rsft\">" . anchor("device/showall/$host_id", 'Runnning Software') . "</li>";
+echo "<li class=\"sto\">". anchor("storage/showall/$host_id", 'Storage') . "</li>";
+echo "<li class=\"isft\">" . anchor("softwareinstalled/showall/$host_id", 'Installed Software') . "</li>";
+echo "<li class=\"rsft\">" . anchor("softwarerunning/showall/$host_id", 'Runnning Software') . "</li>";
 echo "</ul>";
 
 require_once 'footer.php';
