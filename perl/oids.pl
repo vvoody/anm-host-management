8011474ef694388e7d3f0291bffe252020668498
vvoody <ydoovv@gmail.com>
2012-01-16 08:24:43 +0800

    statistics.pl: implemented insert & alarm for storage. oids.pl: added storage OIDs.

--- perl/oids.pl
+++ perl/oids.pl
@@ -4,3 +4,12 @@ $hrDeviceType         = "1.3.6.1.2.1.25.3.2.1.2";
 $hrDeviceDescr        = "1.3.6.1.2.1.25.3.2.1.3";
 $hrDeviceStatus       = "1.3.6.1.2.1.25.3.2.1.5";    # not every entry has this
 $hrDeviceErrors       = "1.3.6.1.2.1.25.3.2.1.6";    # same as above
+
+$hrStorageTable              = "1.3.6.1.2.1.25.2.3";
+$hrStorageIndex              = "1.3.6.1.2.1.25.2.3.1.1";
+$hrStorageType               = "1.3.6.1.2.1.25.2.3.1.2";
+$hrStorageDescr              = "1.3.6.1.2.1.25.2.3.1.3";
+$hrStorageAllocationUnits    = "1.3.6.1.2.1.25.2.3.1.4";
+$hrStorageSize               = "1.3.6.1.2.1.25.2.3.1.5";
+$hrStorageUsed               = "1.3.6.1.2.1.25.2.3.1.6";
+$hrStorageAllocationFailures = "1.3.6.1.2.1.25.2.3.1.7";    # can get, but is hrDeviceIndex
--- perl/statistics.pl
+++ perl/statistics.pl
@@ -89,7 +89,78 @@ sub st_devices {
 
 # do statistics for storage
 sub st_storage {
-    ;
+    my $host = $_[0];
+    my ($host_id, $ip, $community) = @$host;
+    $host_id = int($host_id);
+    print "Now on host $ip...\n";
+    my ($snmp_sess, $err_snmp) = connect_snmp($ip);
+    (&MYLOG($0, "connect_snmp", "$ip", $err_snmp) && return undef) if $err_snmp;
+
+    my ($hash_ref, $err_idxs) = snmp_get_cols($snmp_sess, [$hrStorageIndex]);
+    &MYLOG($0, "snmp_get_cols", "hrStorageIndex", $err_idxs) if $err_idxs;
+    return undef if !defined $hash_ref;
+    my @new_idxs = values %$hash_ref;
+
+    my ($l_ref, $err) = select_table_cols($dbh, "storage", ["id, storage_idx"], $host_id);
+    &MYLOG($0, "select_table_cols", "storage_idx", $err) if $err;
+    return undef if !defined $l_ref;
+
+    my @old_idxs;
+    my %storage;
+    foreach $r (@$l_ref) {
+        my @l = @$r;
+        push @old_idxs, $l[1];   # idx
+        $storage->{$l[1]} = $l[0];  # idx is key, id is value
+    }
+
+    my ($hash_ref, $err_cols) = snmp_get_cols($snmp_sess, [$hrStorageDescr,
+                                                           $hrStorageType,
+                                                           $hrStorageSize,
+                                                           $hrStorageAllocationUnits]);
+    &MYLOG($0, "snmp_get_cols", "stdescr,sttype,stsize,stallocunits", $err_cols) if $err_cols;
+    return undef if !defined $hash_ref;
+
+    my $s_old = Set::Scalar->new(@old_idxs);
+    my $s_new = Set::Scalar->new(@new_idxs);
+
+    # insert new, $s_new - $s_old;
+    my $s_ins =  $s_new - $s_old;
+    print "New storage found: $s_ins\n";
+    while (defined(my $e = $s_ins->each)) {
+        my ($descr, $type, $size, $allocated_sectors) = ($hash_ref->{$hrStorageDescr . ".$e"},
+                                                         $hash_ref->{$hrStorageType . ".$e"},
+                                                         $hash_ref->{$hrStorageSize . ".$e"},
+                                                         $hash_ref->{$hrStorageAllocationUnits . ".$e"});
+        $field_values = {"descr"  => $descr || undef,
+                         "type"   => $type || undef,
+                         "size" => $size || undef,
+                         "allocated_sectors" => $allocated_sectors || undef,
+                         "host_id" => $host_id,
+                         "storage_idx" => $e};
+#        print Dumper($field_values);
+        my $st = insert_hash($dbh, "storage", $field_values);
+        &MYLOG($0, "insert_hash", "devices|$field_values", "insert failed") if !defined $st;
+    }
+
+    # alarm device disappeared, $s_old - $s_new;
+    my $s_notavlb = $s_old - $s_new;
+    # my @m = (35);
+    # my $s_notavlb = Set::Scalar->new(@m);
+    print "alarm not available: $s_notavlb\n";
+    while (defined(my $e = $s_notavlb->each)) {
+        $dbh->do("UPDATE storage SET available = 0 where host_id = $host_id and storage_idx = $e");
+        $field_values = {"tabname" => "storage",
+                         "col" => "available",
+                         "pre_value" => 1,
+                         "now_value" => 0,
+                         "level" => 2,
+                         "tid" => $storage->{$e},
+                         "comment" => "storage $e of host $host_id is not available now!"};
+        my $st = insert_hash($dbh, "statistics", $field_values);
+        &MYLOG($0, "insert_hash", "statistics|$field_values", "insert failed") if !defined $st;
+    }
+
+    $snmp_sess->close();
 }
 
 
@@ -105,8 +176,8 @@ my ($hosts_ref, $err_hosts) = get_hosts($dbh);
 die $err_hosts if $err_hosts;
 
 foreach $host (@$hosts_ref) {
-    st_devices($host);
-    # st_storage($host);
-    # st_software_running($host);
+#    st_devices($host);
+    st_storage($host);
+#    st_software_running($host);
     last;
 }
