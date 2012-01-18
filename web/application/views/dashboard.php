60b8565a38b0425102beab7e791278b0503ae988
vvoody <ydoovv@gmail.com>
2012-01-18 20:09:01 +0800

    New added: views/dashboard.php - at homepage, show some general info;            images/icons/*      - small pictures used at dashboard; Modifed: views/home.php - moved dashboard stuff to view dashboard.php;          css/main.css - added styles for view dashboard;

---
+++ web/application/views/dashboard.php
@@ -0,0 +1,60 @@
+<div id="maincontainer">
+  <div id="main_pane_left">
+    <div class="group">
+      <h2>General Settings</h2>
+      <ul>
+        <li>blablabla</li>
+      </ul>
+    </div>
+    <div class="group">
+      <h2>Appearance Settings</h2>
+      <ul>
+        <li>
+          <h3>You have following alarms in the recent week:</h2>
+          <table border="0" width="30%" class="norm" cellpadding="0" cellspacing="0">
+          <tr>
+            <td>WARNING</td>
+            <td><?php echo anchor('stats/index/warning', $warnings, 'style="text-decoration:underline;"');?></td>
+          </tr>
+          <tr>
+            <td>ERROR</td>
+            <td><?php echo anchor('stats/index/error', $errors, 'style="text-decoration:underline;"');?></td>
+          </tr>
+          <tr>
+            <td>CRITICAL</td>
+            <td><?php echo anchor('stats/index/critical',$criticals, 'style="text-decoration:underline;"');?></td>
+          </tr>
+          </table>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div id="main_pane_right">
+    <div class="group">
+      <h2>Personal Setting</h2>
+      <ul>
+        <li>Hej, xxxx!</li>
+        <li id="li_change_password"><a href="#">Change password</a></li>
+      </ul>
+    </div>
+    <div class="group">
+      <h2>Web server</h2>
+      <ul>
+        <li>Apache/2.2.21 (Unix) DAV/2 PHP/5.3.8</li>
+        <li>MySQL client version: mysqlnd 5.0.8-dev - 20102224 - $Revision: 310735 $</li>
+      </ul>
+    </div>
+    <div class="group">
+      <h2>System Runtime Info</h2>
+        <ul>
+          <li>Version: 1.0</li>
+          <li>Web server: Apache</li>
+          <li>Web server: Apache</li>
+          <li><a href="https://github.com/vvoody/anm-host-management">Source Code</a></li>
+        </ul>
+    </div>
+  </div>
+
+<br class="clearfloat" />
+<br class="clearfloat" />
+</div>
--- web/application/views/home.php
+++ web/application/views/home.php
@@ -1,28 +1,6 @@
 <?php 
 require_once 'header.php';
 require_once 'navigation.php';
-?>
-
-<h2>You have following alarms in the recent week:</h2><br />
-<table border="0" width="40%" class="norm" cellpadding="0" cellspacing="0">
-<tr>
-<th>Level</th>
-<th>Amount</th>
-</tr>
-<tr>
-  <td>WARNING</td>
-  <td><?php echo anchor('stats/index/warning', $warnings, 'style="text-decoration:underline;"');?></td>
-</tr>
-<tr>
-  <td>ERROR</td>
-  <td><?php echo anchor('stats/index/error', $errors, 'style="text-decoration:underline;"');?></td>
-</tr>
-<tr>
-  <td>CRITICAL</td>
-  <td><?php echo anchor('stats/index/critical',$criticals, 'style="text-decoration:underline;"');?></td>
-</tr>
-</table>
-
-<?php
+require_once 'dashboard.php';
 require_once 'footer.php';
 ?>
