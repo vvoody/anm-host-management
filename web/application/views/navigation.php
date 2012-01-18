f1bbd76596aa576a851c763fc388e249e65382f5
vvoody <ydoovv@gmail.com>
2012-01-18 22:21:09 +0800

    Hosts and Users pages won't be shown to normal user.

--- web/application/views/navigation.php
+++ web/application/views/navigation.php
@@ -12,13 +12,15 @@ echo anchor('stats/', 'Statistics');
 
 echo "<span>&bull;</span>";
 
-echo anchor('host/show/all', 'Hosts');
-
-echo "<span>&bull;</span>";
-
-echo anchor('user/show/all', 'Users');
-
-echo "<span>&bull;</span>";
+if ($this->session->userdata('isAdmin') == TRUE) {
+    echo anchor('host/show/all', 'Hosts');
+    
+    echo "<span>&bull;</span>";
+    
+    echo anchor('user/show/all', 'Users');
+    
+    echo "<span>&bull;</span>";
+}
 
 echo anchor('master/logout', 'Logout');
 
